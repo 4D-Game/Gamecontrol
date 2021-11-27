363f4c87c2399799c1cee11ec7a48b1f2ec7dcc3
lukson83 <Sonja.Lukas@edu.fh-joanneum.at>
2021-11-27 20:47:10 +0100

    new start

--- src/controls/stepper.py
+++ src/controls/stepper.py
@@ -13,16 +13,22 @@ from ..hardware.stepperHAL import StepperHAL
 import time
 import atexit 
 import logging
+import threading
 import mqtt
 
-class Stepper(): 
+class Stepper(StepperHAL): 
     """
         Gamecontroll Stepper
-
     """
     def __init__(self):
+        super(). __init__(self, port_name, port_number:int, rpm:int, addr=0x60)    
+    def set_speed(self): 
         pass
+    def get_points(): #???
+        pass     
+    def shutdown():
+        pass 
 
-    async def shutdown(self):
-        pass
+       
+
 

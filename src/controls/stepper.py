1d9c88e89deb154d64565940dafa0649dd607072
lukson83 <Sonja.Lukas@edu.fh-joanneum.at>
2021-12-08 20:39:03 +0100

    new codes with lib: motorkit and async

--- src/controls/stepper.py
+++ src/controls/stepper.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python3
 
 """
-Created: 11/22/21
+Created: 12/08/21
 by: Sonja Lukas
 Tower control with 2 stepper
-Game part
+Motorkit version
 """
-
-#from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
-from Adafruit_MotorHAT.Adafruit_MotorHAT_Motors import Adafruit_MotorHAT
+from adafruit_motor import stepper as STEPPER
+from adafruit_motorkit import MotorKit
 from ..hardware.stepperHAL import StepperHAL
 
 import time
 import atexit 
 import logging
-import threading
 import asyncio
-import queue
+import board
 
 class Stepper(StepperHAL): #Control
     """
         Gamecontroll Stepper
     """
-    def __init__(self,stepper_name, port_number, rpm:int):
-        super(). __init__(stepper_name, port_number, rpm) 
+
+    def __init__(self, stepper_name, port_number):
+        """
+        instantiation of the 2 steppers 
+        """
+        #self.stepper1 = StepperHAL("myStepper1",1) #Conversion open, for test still different 
+        #self.stepper2 = StepperHAL("myStepper2",2)
+        super(). __init__(stepper_name, port_number)
 
     async def set_start_position(self):
         """
             set start position after each game round 
         """
-        await self.set_position(100)
+        await self.set_position(100) #definition open
 
     async def get_score(self):
         """
@@ -39,36 +43,62 @@ class Stepper(StepperHAL): #Control
         # on end:
         # await self.motor_stop() # todo: change in on_end at game_sdk
         pass
+
+    async def set_values(self, new_sleepy, new_dir):
+        """
+            set variable 
+        """
+        #todo: create a function to value transfer 
+        self.sleepy=new_sleepy
+        self.direction=new_dir
+
     async def game_run(self):
         """
             game starts in game sdk
             mapping attributes to rotate()
-        """ 
-        #start=time.time()
-         
-        if self.port_number==2: #test put 3 in
-            self.thread_queue.put({'rpm': 20, 'step_size': 100, 'direction': Adafruit_MotorHAT.FORWARD})
-            self.thread_queue.put({'rpm': 20, 'step_size': 100, 'direction': Adafruit_MotorHAT.BACKWARD})
-            self.thread_queue.put({'rpm': 30, 'step_size': 100, 'direction': Adafruit_MotorHAT.FORWARD})
-        
-        if self.port_number==1:
-            self.thread_queue.put({'rpm': 20, 'step_size': 50, 'direction': Adafruit_MotorHAT.FORWARD}) 
-            self.thread_queue.put({'rpm': 20, 'step_size': 50, 'direction': Adafruit_MotorHAT.BACKWARD})
+        """    
+        while True:
+            print("test I´m in game_run loop") #ok, ok
+            await self.rotate() #ok, ok 
+            
+            if self.name=='myStepper2': #not ok!!!! 
+                self.new_dir=STEPPER.FORWARD                  #No change of direction at Step2            
+                self.new_sleepy=0.04
+                await self.set_values(self.new_sleepy, self.new_dir)
+            
+            if self.name=='myStepper1':
+                start=time.time()
+                duration = await time.time()-start
+                print(f"{duration}")
+                if duration>2:
+                    self.new_sleepy=0.05
+                    if self.new_dir==STEPPER.FORWARD:
+                        self.new_dir=STEPPER.BACKWARD
+                    else:
+                        self.new_dir=STEPPER.FORWARD       
+                    await self.set_values(self.new_sleepy, self.new_dir)
+                    duration=0                
 
-        await self.rotate()
+        #logging.info(f"set_start_position Duration: {duration}")         
 
-        #duration = time.time()-start
-        #logging.info(f"set_start_position Duration: {duration}")  
-                        
     async def on_exit(self):
         """
             threading stop, motor_release via atexit register 
         """
-        await self.close()    #todo: check hal.close
+        await self.set_start_position() 
+        self.close()    #todo: check hal.close
 
+Step1 = Stepper("myStepper1",1) #for tests 
+Step2 = Stepper("myStepper2",2)
+
+async def main():
+    await asyncio.gather((Step2.game_run()),(Step1.game_run()))
 
 if __name__ == "__main__":
-    Stepper1= Stepper("myStepper1",1,20)      #port_name, port_number, rpm_init 
-    Stepper2= Stepper("myStepper2",2,20) 
-    #asyncio.run(Stepper2.game_run())
-    asyncio.run(Stepper1.game_run())
+    asyncio.run(main())
+
+    #testloop.close()   #check:                                            
+    #set_start_position()
+    #get_positon()
+    #set_position()
+    #motor_stop()
--- src/hardware/stepperHAL.py
+++ src/hardware/stepperHAL.py
@@ -1,52 +1,42 @@
 #!/usr/bin/env python3
 
 """
-Created: 11/22/21
+Created: 12/08/21
 by: Sonja Lukas
 Tower control with 2 stepper
-Hardware part
+Hardware part 
+version MotorKit
 """
 import time
 import atexit 
-import logging
-import threading
 import asyncio
-import queue
+import board
+import logging
 
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+from adafruit_motor import stepper as STEPPER
+from adafruit_motorkit import MotorKit
 
 class StepperHAL():
-    """
-        Hardware interface of tower
-        Attributes:
-            stepper_name: for port M1_M2 is equal to port_number 1
-                       for port M3_M4 is equal to port_number 2
-            port_number: only 1 or 2!
-            rpm: set speed () - specified to between 0 - 30, increase possible
-            addr: I2C address of the motor HAT, default is 0x60
-        Power specified at hardware: 
-            port_number 1: rock 
-            port_number 2: rotate, both directions possible
-        Style:
-            specified on DOUBLE, others possible
-        Threads:  thread_St1 and thread_St2   
-    """ 
-
-    def __init__(self, stepper_name, port_number, rpm, addr=0x60, stop_at_exit=True): 
+    #step_style=STEPPER.DOUBLE
+    def __init__(self, stepper_name, port_number, stop_at_exit=True): 
         """
-            with '_station' create an MotorHAT Object
+            with '_station' create an MotorKit Object
             'turn_off_motor' required for auto-disabling motors on shutdown
         """
-        self._station=Adafruit_MotorHAT(addr)    #composition attribute
-        atexit.register(self.turn_off_motor) 
-        self.steps=1                             #default step size    
-        self.name=stepper_name                   #gets myStepper1 and myStepper2
-        self.port_number=port_number             #only 1 or 2!
-        self.motor=self._station.getStepper(200, self.port_number)   # 200 steps/rev, motor port #1 oder #2
-        self.motor.setSpeed(rpm) 
-        self.stepper_thread=threading.Thread()   #init empty threads
-        self.thread_queue=queue.Queue()
-        self.queue_dict= ({'rpm': 20, 'step_size': 0, 'direction': Adafruit_MotorHAT.FORWARD}) #init only
+        self._station=MotorKit(i2c=board.I2C())    #composition attribute
+        if stop_at_exit:
+            atexit.register(self.turn_off_motor)   #self.stop 
+        self.name=stepper_name                     #gets myStepper1 and myStepper2
+        self.port_number=port_number               #only 1 or 2!
+    
+        if port_number==1:
+            self.motor=self._station.stepper1 
+        elif port_number==2:
+            self.motor=self._station.stepper2
+
+        self.sleepy=0.05  #naming open #without self test open #default=0.05
+        self.direction=STEPPER.FORWARD #default=FORWARD
+        self.step_style=STEPPER.DOUBLE #default=DOUBLE   
 
     async def get_position(self):
         """
@@ -57,114 +47,82 @@ class StepperHAL():
         print(f"current position is {position}")
         return int(position)
 
-    async def set_position(self, pos_goal, rpm:int=20 ) -> int:
+    async def set_position(self, pos_goal) -> int:
         """
             set stepper position for game start
             Arguments:  pos_goal
                         rpm
         """ 
-        self.motor.setSpeed(rpm)         #rpm at setup default=10
         pos_current=await self.get_position()
         
         logging.info(f"Port {self.name} current position {pos_current}")
-        
-        steps=pos_current-pos_goal      #calc step size    
+
+        steps:int=pos_current-pos_goal      #calc step size    
         if steps>0:
-            self.motor.step(steps, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.DOUBLE)
             print(f"steps to go {steps}")
+            for step in range(steps):
+                self.motor.onestep(direction=STEPPER.BACKWARD, style=self.step_style)
+                await asyncio.sleep(0.05)
         elif steps<0:
-            self.motor.step(abs(steps), Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
-            print(f"steps to go {steps}")
+            for step in range(abs(steps)):
+                self.motor.onestep(direction=STEPPER.FORWARD, style=self.step_style)
+                await asyncio.sleep(0.05)
         
         pos_current = await self.get_position()
         logging.info(f"{self.name} check current position {pos_current} is equal to {pos_goal}?")
         print(f"{self.name} check current position {pos_current} is equal to {pos_goal}")
-        return pos_current    
+        return pos_current 
 
-    def _stepper_worker(self, thread_queue):
+    async def _stepper_worker(self, direction):
         """
-            Thread worker
-            Attributes: queue dict[ steps,
-                                    direction,
-                                    rpm]
+
         """
-        #while self.thread_queue.empty==False:
-        self.queue_dict=self.thread_queue.get()
-        #self.queue_dict=self.thread_queue.get()
-        print(self.queue_dict)       
-        logging.info(f"dictionary: {self.queue_dict}") 
-        self.motor.setSpeed(self.queue_dict['rpm'])
-        self.motor.step(self.queue_dict['step_size'], self.queue_dict['direction'], Adafruit_MotorHAT.DOUBLE)
+        #while True: 
+        #    self.motor.onestep(direction=STEPPER.FORWARD, style=STEPPER.DOUBLE)                
+        #    logging.info(f"stepper worker doc: Direction {self.direction} ") 
+        #    await asyncio.sleep() #asyncio sleep
+        pass 
 
     async def rotate(self):    
         """
-        thread control
-
-        """
+            thread control
+        """    
         while True:
-            if not self.stepper_thread.isAlive(): 
-                self.stepper_thread=threading.Thread(target=self._stepper_worker, args=(self.thread_queue,)) 
-                self.stepper_thread.start()
-                logging.info(f"create and start Thread {self.name} in {self.stepper_thread}")
-                print(f"create and start Thread {self.name} in {self.stepper_thread} with {self.thread_queue}")      
-
+            self.motor.onestep(direction=self.direction, style=self.step_style)
+            await asyncio.sleep(self.sleepy)
+            logging.info(f"rotate: {self.name} direction: {self.direction} with sleeptime {self.sleepy}")
+            #print(f"rotate {self.name} {self.direction} with {self.spleepy}")   
+    
     async def motor_stop(self):
         """
-            motor hold 
+            single motor hold for play algorithm
         """
-        if self.port_number == 1:
-            self._station.getMotor(1).run(Adafruit_MotorHAT.BRAKE)
-            self._station.getMotor(2).run(Adafruit_MotorHAT.BRAKE)
-            logging.info(f"{self.name} stop")
-        elif self.port_number == 2:
-            self._station.getMotor(3).run(Adafruit_MotorHAT.BRAKE)
-            self._station.getMotor(4).run(Adafruit_MotorHAT.BRAKE)
-            logging.info(f"Port {self.name} stop") 
-        else:
-            logging.info(f"Motor {self.name} don´t stop")          
+        #self.motor.release()
+        #Distinction should not be necessary, test open, hedging? 
         
+        if self.name=='myStepper1':
+            self.motor.release()
+            print(f"Motor {self.name} stop")
+            logging.info(f"Motor {self.name} stop")
+        elif self.name=='myStepper2':
+            self.motor.release()
+            print(f"Motor {self.name} stop")
+            logging.info(f"Motor {self.name} stop") 
+        else:
+            logging.info(f"Motor {self.name} doesn´t stop") 
+        
+   
     def turn_off_motor(self):
         """
             Auto-disabling motors on shutdown   
         """
-        self._station.getMotor(1).run(Adafruit_MotorHAT.RELEASE) 
-        self._station.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-        self._station.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-        self._station.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+        self.motor.release()
+        #self.release() #2nd test option 
         logging.info(f"Check on hardware: both ports release")
         
     async def close(self):
         """
            call in superordinate structure
         """
-        #self.stepper_thread.stop()            #don't work
-        #self.stepper_thread.shutdown = True   #noch nicht geschrieben           
+        await self.motor_stop()         
         logging.info(f"stepperHAL close function: {self.name} close")
-
-
-class EncoderHAL():
-    def __init__(self, name): 
-        self.name=name
-        self.motor_steps_per_rev=200
-        self.motor_step_angle=1,8
-
-    async def imp_count(self):
-        pass     
-
-
-     
-
-
-#if __name__ == "__main__":
-    #Stepper1= StepperHAL("myStepper1",1,20) #port_name, port_number, rpm_init 
-    #Stepper2= StepperHAL("myStepper2",2,20) 
-    #tests:
-    #asyncio.run(Stepper1.get_position())                 #ok test get_position
-    #asyncio.run(Stepper2.get_position())  
-    #asyncio.run(Stepper1.set_position(0))                 #ok test set_position, both directions 
-    #asyncio.run(Stepper2.set_position(0)) 
-    #asyncio.run(Stepper2.rotate(Adafruit_MotorHAT.FORWARD, 20, 500)) #ok test threads manual
-    #asyncio.run(Stepper1.rotate(Adafruit_MotorHAT.FORWARD, 10, 100)) 
-    #asyncio.run(Stepper1.motor_stop())                               #single function test ok
-    #asyncio.run(Stepper2.motor_stop())
-    

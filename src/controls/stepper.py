47f353963d36526a2f8353e7f279440f09d4ebab
lukson83 <Sonja.Lukas@edu.fh-joanneum.at>
2022-01-12 10:53:35 +0000

    current revision

--- src/controls/stepper.py
+++ src/controls/stepper.py
@@ -6,46 +6,46 @@ by: Sonja Lukas
 Tower control with 2 stepper
 Motorkit version
 """
-from asyncio.tasks import current_task
-from typing import Literal
-from adafruit_motor import stepper as STEPPER
-from adafruit_motorkit import MotorKit
-from hardware.stepper_hal import StepperHAL, Encoder
-
-import time
-import atexit
 import logging
 import asyncio
-import RPi.GPIO as GPIO
 import random
+
+from typing import Literal
+from adafruit_motor import stepper as STEPPER
+from hardware.stepper_hal import EncoderStepperHAL, StepperHAL
+#from asyncio.tasks import current_task
 
 class TowerControl():
     """
         Gamecontroll Stepper
-    """
 
-    # max_angle
-    # min_angle
-    # max_interval
-    # min_interval
+            Attributes:
+                angle_max: limit rocking forward stepper1
+                angle_min: limit rocking backward stepper1
+                interval_max: max. waiting time rotate stepper 2
+                interval_min: min. waiting time rotate stepper 2
+    """
+    max_angle=20
+    min_angle=-20
+    max_interval=0.05
+    min_interval=0.01
 
     def __init__(self):
         """
             instantiation of the 2 steppers and encoder for rocking stepper
 
         """
-        self.stepper1 = StepperHAL("myStepper1",1)
+        self.stepper1 = EncoderStepperHAL("myStepper1",1)
         self.stepper2 = StepperHAL("myStepper2",2)
-        self.encoder=Encoder()
         self.total_score=0
-        self.encoder.enc_imp=0
-        self.score_goal=int(500) #todo::::: call sdk definition
+        self.stepper1.encoder.enc_imp=0 
+        self.score_goal=int(50) #todo::::: call sdk definition
 
     async def set_start_position(self):
         """
             set start position after each game round
         """
-        steps=self.encoder.enc_imp
+        steps=self.stepper1.encoder.enc_imp
         await self.stepper1.set_position(steps) #definition open
 
     async def get_score(self):
@@ -63,9 +63,9 @@ class TowerControl():
         #for sim only
         if self.total_score<self.score_goal:
             self.total_score=self.total_score+1
+            await asyncio.sleep(2)
         else:
             self.total_score=0
-
         return self.total_score
 
     async def game_run(self):
@@ -101,23 +101,23 @@ class TowerControl():
                 angle_min: limit backward rocking value, definition in class stepperHAL
         """
         interval=random.uniform(0.01,0.05)
-        logging.info(f"function call stepper1_algo, random interval: {interval}")
+        logging.debug(f"function call stepper1_algo, random interval: {interval}")
 
         old_direction:Literal=self.stepper1.direction
-        angle = await self.encoder.angle_calc()
-        logging.info(f"stepper1_algo get angle {angle}")
-        if angle >= self.stepper1.angle_max:
+        angle = self.stepper1.encoder.angle_calc()
+        logging.debug(f"stepper1_algo get angle {angle}")
+        if angle >= self.max_angle:
             if old_direction==STEPPER.FORWARD:
                 new_dire=STEPPER.BACKWARD
-                logging.info(f"direction changes: {STEPPER.BACKWARD}, Trigger: angle_max")
+                logging.info(f"direction changes: {STEPPER.BACKWARD}, Trigger: max_angle")
                 return interval, new_dire
             else:
                 logging.info(f"ERROR Trigger: angle_max")
 
-        elif angle <= self.stepper1.angle_min:
+        elif angle <= self.min_angle:
             if old_direction==STEPPER.BACKWARD:
                 new_dire=STEPPER.FORWARD
-                logging.info(f"direction changes: {STEPPER.FORWARD}, Trigger: angle_min")
+                logging.info(f"direction changes: {STEPPER.FORWARD}, Trigger: min_angle")
                 return interval, new_dire
             else:
                 logging.info(f"ERROR Trigger: angle_min")
@@ -142,7 +142,7 @@ class TowerControl():
         direction:Literal=self.stepper2.direction
         current_score=await self.get_score()
 
-        interval=await self.score_map(current_score, 0, self.score_goal, self.stepper2.interval_max, self.stepper2.interval_min)
+        interval=await self.score_map(current_score, 0, self.score_goal, self.max_interval, self.min_interval)
         logging.info(f"function call stepper2_algo, score: {current_score} mapping to interval {interval}")
         return interval, direction
 
